b8e93df8a9c293a850ba3b516a1f868d771ed1ba
technus <jhoan199412@gmail.com>
2021-03-02 20:30:46 -0500

    correción de texto en index

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,3 +36,34 @@ btn_show_is_scroll_page_awake = function()
 btn_show_is_scroll_page_awake();
 //----
 
+let btn_menu = document.querySelector('.btn-menu');
+let navbar_links = document.querySelector('.navbar-links');
+menu_open_load = function(){
+ 
+
+  btn_menu.onclick = function(){
+    if(btn_menu.classList.contains('is-open')){
+      btn_menu.classList.remove('is-open');
+      navbar_links.classList.remove('is-open');
+    }else{
+      btn_menu.classList.add('is-open');
+      navbar_links.classList.add('is-open');
+    }
+  }
+}
+
+menu_close_item_laod = function(){
+  nav_items = document.querySelectorAll('.navbar-nav>.nav-item');
+  
+  nav_items.forEach(element => {
+    element.onclick = function()
+    {
+      btn_menu.classList.remove('is-open');
+      navbar_links.classList.remove('is-open');
+
+    }
+  });
+}
+
+menu_open_load();
+menu_close_item_laod();
